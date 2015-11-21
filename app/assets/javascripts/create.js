7679d33f6e39ada3511a160b8de323a607e4b444
wataru0225 <wataru.kikuchi0225@gmail.com>
2015-11-22 06:48:40 +0900

    [add]検索したのを表示

--- app/assets/javascripts/create.js
+++ app/assets/javascripts/create.js
@@ -1,18 +1,18 @@
 var createTopic = function($scope, $http) {
-  $scope.events = [];
 
   $("#research_word").keypress(function (e) {
     if (e.which == 13) {
       if ($scope.researchWord) {
-        getEvent($scope.researchWord, $scope.events);
+        getEvent($scope.researchWord);
       }
     }
   });
 
-  function getEvent(keyword, events) {
+  function getEvent(keyword) {
     $http.jsonp("http://connpass.com/api/v1/event/?callback=JSON_CALLBACK&keyword=" + keyword)
     .success(function(data) {
       console.log(data["events"]);
+      $scope.events = data["events"];
     })
     .error(function() {
       console.log("error!");
